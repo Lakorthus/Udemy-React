b1c1641a9560efa1dec344b1a36ecfefe0285a40
Lakorthus <julio28ve@gmail.com>
2022-07-06 15:51:54 +0100

    StartAgain

---
+++ RepasoNivelacion/map.js
@@ -0,0 +1,11 @@
+const datos = [
+    {nombre: 'Julio',edad: 32},
+    {nombre: 'Jlio',edad: 60},
+    {nombre: 'Juio',edad: 32},
+    {nombre: 'Jio',edad: 15},
+    {nombre: 'Jo',edad: 20}
+]
+
+datos.map(elemento => {
+    console.log(elemento.nombre)
+})
