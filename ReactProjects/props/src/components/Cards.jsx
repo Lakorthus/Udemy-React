02e4637d54c1af8d66bc70c6a0dc2ff0a0d04635
Lakorthus <julio28ve@gmail.com>
2022-07-08 16:59:22 +0100

    cards

--- ReactProjects/props/src/components/Cards.jsx
+++ ReactProjects/props/src/components/Cards.jsx
@@ -10,9 +10,9 @@ const Cards = (props) => {
       <div className="card-body">
         <h5 className="card-title">{titulo}</h5>
         <p className="card-text">{texto}</p>
-        <a href="#" className="btn btn-primary">
+        <button className="btn btn-primary">
           Ver mas
-        </a>
+        </button>
       </div>
     </div>
   );
