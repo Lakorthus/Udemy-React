290ba49e7c660501f2aa0889362384db472f5e31
Lakorthus <julio28ve@gmail.com>
2022-07-09 12:14:53 +0100

    useParams

--- ReactProjects/routers/src/App.jsx
+++ ReactProjects/routers/src/App.jsx
@@ -7,10 +7,11 @@ const App = () => {
   return (
     <Router>
       <Link to={'/inicio'}>Inicio</Link>
+      <br/>
       <Link to={'/'}>Base</Link>
 
       <Routes>
-        <Route path="/inicio">
+        <Route path="/inicio/:nombre"> //se puedeb crear mas rutas /:x/:x y hacer mix cons static /:x/y
           <Route index element={<Inicio />} />
         </Route>
         <Route path="/">
--- ReactProjects/routers/src/components/Inicio.jsx
+++ ReactProjects/routers/src/components/Inicio.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const Inicio = () => {
+
+  const {nombre} = useParams()
   return (
     <div>
         <h1>Pagina de Inicio</h1>
+        {nombre}
     </div>
   )
 }
