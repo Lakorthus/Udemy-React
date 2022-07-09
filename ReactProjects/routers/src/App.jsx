06fe2163b08b28578129a438303566356e39fc43
Lakorthus <julio28ve@gmail.com>
2022-07-09 13:51:06 +0100

    axios y Apis

--- ReactProjects/routers/src/App.jsx
+++ ReactProjects/routers/src/App.jsx
@@ -1,26 +1,43 @@
 import React from "react";
-import Inicio from "./components/Inicio";
-import Base from "./components/Base";
+// import Inicio from "./components/Inicio";
+// import Base from "./components/Base";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-
+import Usuarios from "./components/Usuarios";
+import Usuario from "./components/Usuario";
 const App = () => {
   return (
-    <Router>
-      <Link to={"/inicio"}>Inicio</Link>
-      <br />
-      <Link to={"/"}>Base</Link>
+    // Tema de Route
+    // <Router>
+    //   <Link to={"/inicio"}>Inicio</Link>
+    //   <br />
+    //   <Link to={"/"}>Base</Link>
 
-      <Routes>
-        <Route path="/inicio/:nombre">
-          {" "}
-          //se puedeb crear mas rutas /:x/:x y hacer mix cons static /:x/y
-          <Route index element={<Inicio />} />
-        </Route>
-        <Route path="/">
-          <Route index element={<Base />} />
-        </Route>
-      </Routes>
-    </Router>
+    //   <Routes>
+    //     <Route path="/inicio/:nombre">
+    //       {" "}
+    //       //se puedeb crear mas rutas /:x/:x y hacer mix cons static /:x/y
+    //       <Route index element={<Inicio />} />
+    //     </Route>
+    //     <Route path="/">
+    //       <Route index element={<Base />} />
+    //     </Route>
+    //   </Routes>
+    // </Router>
+
+    <div>
+      <Router>
+        <Link to={"/"}>Usuarios</Link>
+
+        <Routes>
+          <Route path="/">
+            <Route index element={<Usuarios />} />
+          </Route>
+          <Route path="/usuario/:id">
+            <Route index element={<Usuario />} />
+          </Route>
+        </Routes>
+      </Router>
+    </div>
   );
 };
 
