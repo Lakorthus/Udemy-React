e5836ad0e9a4cffe25aee9639ba6be9475cda4be
Lakorthus <julio28ve@gmail.com>
2022-07-09 12:01:01 +0100

    useEffect

--- ReactProjects/routers/src/components/Base.jsx
+++ ReactProjects/routers/src/components/Base.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 const Base = () => {
+  const [nombre, setNombre] =  useState('Pedro')
+  useEffect( ()=>{
+      setTimeout(()=>{
+        setNombre('Manuel') 
+      },2000) // 2 segundos
+  })
+
   return (
     <div>
         <h1>Pagina de base ruta /</h1>
+        {nombre}
     </div>
   )
 }
