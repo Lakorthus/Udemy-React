d749c3f0955eb976a42669c757c50a6d3fdaf083
Lakorthus <julio28ve@gmail.com>
2022-07-09 12:15:20 +0100

    format

--- ReactProjects/routers/src/App.jsx
+++ ReactProjects/routers/src/App.jsx
@@ -6,12 +6,14 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 const App = () => {
   return (
     <Router>
-      <Link to={'/inicio'}>Inicio</Link>
-      <br/>
-      <Link to={'/'}>Base</Link>
+      <Link to={"/inicio"}>Inicio</Link>
+      <br />
+      <Link to={"/"}>Base</Link>
 
       <Routes>
-        <Route path="/inicio/:nombre"> //se puedeb crear mas rutas /:x/:x y hacer mix cons static /:x/y
+        <Route path="/inicio/:nombre">
+          {" "}
+          //se puedeb crear mas rutas /:x/:x y hacer mix cons static /:x/y
           <Route index element={<Inicio />} />
         </Route>
         <Route path="/">
@@ -19,7 +21,6 @@ const App = () => {
         </Route>
       </Routes>
     </Router>
-    
   );
 };
 
--- ReactProjects/routers/src/components/Base.jsx
+++ ReactProjects/routers/src/components/Base.jsx
@@ -1,19 +1,19 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 
 const Base = () => {
-  const [nombre, setNombre] =  useState('Pedro')
-  useEffect( ()=>{
-      setTimeout(()=>{
-        setNombre('Manuel') 
-      },2000) // 2 segundos
-  })
+  const [nombre, setNombre] = useState("Pedro");
+  useEffect(() => {
+    setTimeout(() => {
+      setNombre("Manuel");
+    }, 2000); // 2 segundos
+  });
 
   return (
     <div>
-        <h1>Pagina de base ruta /</h1>
-        {nombre}
+      <h1>Pagina de base ruta /</h1>
+      {nombre}
     </div>
-  )
-}
+  );
+};
 
-export default Base
+export default Base;
