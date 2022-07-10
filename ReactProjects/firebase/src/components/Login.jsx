7b02eae2bb51af2a96dd099fb2bda0f09252f47f
Lakorthus <julio28ve@gmail.com>
2022-07-10 02:00:04 +0100

    fire-base

--- ReactProjects/firebase/src/components/Login.jsx
+++ ReactProjects/firebase/src/components/Login.jsx
@@ -1,9 +1,46 @@
-import React from "react";
+import React, {useState} from "react";
+import { auth } from "../firebase-cofing";
 
 const Login = () => {
+
+    const [email, setEmail] = useState ('')
+    const [pass, setPass] = useState ('')
+
+    const registrarUsurio =(e)=>{
+        e.preventDefault()
+        try{
+            auth.createUserWithEmailAndPass(email,pass)
+            alert('Usuario Registrado')
+        }catch(e){
+            console.log(e)
+        }
+    }   
+
   return (
-    <div>
-      <h2>Login</h2>
+    <div className="row mt-5">
+      <div className="col"></div>
+      <div className="col">
+        <form className="form-group d-grid">
+          <input
+            onChange={(e)=>{setEmail(e.target.value)}}
+            className="form-control"
+            placeholder="Introduce el Email"
+            type="email"
+          />
+          <input
+            onChange={(e)=>{setPass(e.target.value)}}
+            className="form-control mt-2"
+            placeholder="Introduce el Password"
+            type="text"
+          />
+          <input
+            className="btn btn-primary mt-4  "
+            value={"Registrar Usuario"}
+            type="submit"
+          />
+        </form>
+      </div>
+      <div className="col"></div>
     </div>
   );
 };
