9ffcdb8d8a4e4802fd0b02c8dd5a71d3291b131e
Lakorthus <julio28ve@gmail.com>
2022-07-11 14:09:42 +0100

    botones conectados a la pase de datos

--- ReactProjects/firestore-datos/src/App.jsx
+++ ReactProjects/firestore-datos/src/App.jsx
@@ -1,45 +1,111 @@
 import React, { useState, useEffect } from "react";
-import { store } from "./firebase-config"
+import { store } from "./firebase-config";
 
 function App() {
+  const [modoedicion, setModoEdicion] = useState(null);
+  const [idusuario, setIdUsuario] = useState("");
   const [nombre, setNombre] = useState("");
   const [phone, setPhone] = useState("");
   const [error, setError] = useState("");
-  const [usuario, setUsuario] = useState("");
+  const [usuariosagenda, setUsuariosAgenda] = useState("");
 
-  const setUsuarios = async (e)=>{
-    e.preventDefault()
-    if(!nombre.trim()){
-      setError('El campo nombre esta vacio')
+  useEffect(() => {
+    const getUsuarios = async () => {
+      const { docs } = await store.collection("agenda").get();
+      const nuevoArray = docs.map((item) => ({ id: item.id, ...item.data() }));
+      setUsuariosAgenda(nuevoArray);
+    };
+    getUsuarios();
+  }, []);
+
+  const setUsuarios = async (e) => {
+    e.preventDefault();
+    if (!nombre.trim()) {
+      setError("El campo nombre esta vacio");
     }
-    if(!phone.trim()){
-      setError('El campo telefono esta vacio')
+    if (!phone.trim()) {
+      setError("El campo telefono esta vacio");
     }
     const usuario = {
-      nombre:nombre,
-      telefono:phone
+      nombre: nombre,
+      telefono: phone,
+    };
+    try {
+      const data = await store.collection("agenda").add(usuario);
+      const { docs } = await store.collection("agenda").get();
+      const nuevoArray = docs.map((item) => ({ id: item.id, ...item.data() }));
+      setUsuariosAgenda(nuevoArray);
+      alert("Friend added");
+    } catch (e) {
+      console.log(e);
     }
-    try{
+    setNombre("");
+    setPhone("");
+  };
 
-      const data = await store.collection('agenda').add(usuario)
-      console.log('tarea added')
-    }catch(e){
-      console.log(e)
+  const borrarUsuario = async (id) => {
+    try {
+      await store.collection("agenda").doc(id).delete();
+      const { docs } = await store.collection("agenda").get();
+      const nuevoArray = docs.map((item) => ({ id: item.id, ...item.data() }));
+      setUsuariosAgenda(nuevoArray);
+    } catch (e) {
+      console.log(e);
     }
-    setNombre('')
-    setPhone('')
+  };
 
+  const pulsarActualizar = async (id) => {
+    try {
+      const data = await store.collection("agenda").doc(id).get();
+      const { nombre, telefono } = data.data();
+      setNombre(nombre);
+      setPhone(telefono);
+      setIdUsuario(id);
+      setModoEdicion(true);
+      console.log(id);
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
-  }
+  const setUpdate = async (e) => {
+    e.preventDefault();
+    if (!nombre.trim()) {
+      setError("El campo nombre esta vacio");
+    }
+    if (!phone.trim()) {
+      setError("El campo telefono esta vacio");
+    }
+    const userUpdate = {
+      nombre: nombre,
+      telefono: phone,
+    };
+    try {
+      await store.collection("agenda").doc(idusuario).set(userUpdate);
+      const { docs } = await store.collection("agenda").get();
+      const nuevoArray = docs.map((item) => ({ id: item.id, ...item.data() }));
+      setUsuariosAgenda(nuevoArray);
+      alert("Friend Edit");
+    } catch (e) {
+      console.log(e);
+    }
+    setNombre("");
+    setPhone("");
+    setIdUsuario("");
+    setModoEdicion(false);
+  };
 
   return (
     <div className="container">
       <div className="row">
         <div className="col">
           <h2>Formulario de Usuarios</h2>
-          <form onSubmit={setUsuarios} className="form-group d-grid">
+          <form
+            onSubmit={modoedicion ? setUpdate : setUsuarios}
+            className="form-group d-grid"
+          >
             <input
-            value={nombre}
+              value={nombre}
               onChange={(e) => {
                 setNombre(e.target.value);
               }}
@@ -48,7 +114,7 @@ function App() {
               placeholder="Introduce el nombre"
             />
             <input
-            value={phone}
+              value={phone}
               onChange={(e) => {
                 setPhone(e.target.value);
               }}
@@ -56,25 +122,51 @@ function App() {
               className="form-control mt-2"
               placeholder="Introduce el numero"
             />
-            <input
-              type="submit"
-              className="btn btn-dark mt-3"
-              value={"Registrar"}
-            />
+            {modoedicion ? (
+              <input
+                type="submit"
+                className="btn btn-info mt-3"
+                value={"Editar"}
+              />
+            ) : (
+              <input
+                type="submit"
+                className="btn btn-dark mt-3"
+                value={"Registrar"}
+              />
+            )}
           </form>
-          {
-            error ?
-            (
-              <div>{error}</div>
-            )
-            :
-            (
-              <span></span>
-            )
-          }
+          {error ? <div>{error}</div> : <span></span>}
         </div>
         <div className="col">
           <h2>Lista de tu agenda</h2>
+          <ul className="list-group">
+            {usuariosagenda.length != 0 ? (
+              usuariosagenda.map((item) => (
+                <li className="list-group-item" key={item.id}>
+                  <strong>{item.nombre}</strong> -- {item.telefono}
+                  <button
+                    onClick={(id) => {
+                      borrarUsuario(item.id);
+                    }}
+                    className="btn btn-danger float-end px-3"
+                  >
+                    Borrar
+                  </button>
+                  <button
+                    onClick={(id) => {
+                      pulsarActualizar(item.id);
+                    }}
+                    className="btn btn-primary float-end me-2"
+                  >
+                    Actualizar
+                  </button>
+                </li>
+              ))
+            ) : (
+              <span>Lo siento no hay usuarios en tu agenda</span>
+            )}
+          </ul>
         </div>
       </div>
     </div>
