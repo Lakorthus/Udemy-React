6fc0687397232239c57f2c00629a379ed541159a
Lakorthus <julio28ve@gmail.com>
2022-08-03 17:51:55 +0100

    firebase-key-removed

--- ReactProjects/firestore-datos/src/firebase-config.js
+++ ReactProjects/firestore-datos/src/firebase-config.js
@@ -9,13 +9,7 @@ import 'firebase/compat/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBcHxiwyG2evTiNDfjsaRNwaK-Xs7Rs_dc",
-  authDomain: "prueba-udemy-react.firebaseapp.com",
-  projectId: "prueba-udemy-react",
-  storageBucket: "prueba-udemy-react.appspot.com",
-  messagingSenderId: "7036165806",
-  appId: "1:7036165806:web:c38cbe9fe350f82569d73f",
-  measurementId: "G-L13DPLGMDW"
+ 
 };
 
 // Initialize Firebase
