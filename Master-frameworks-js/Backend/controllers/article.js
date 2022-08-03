14eb3e1feeb1cc71d2ff55942d6f6be16649ee9b
Lakorthus <julio28ve@gmail.com>
2022-08-03 11:50:17 +0100

    metodo delete and edit

--- Master-frameworks-js/Backend/controllers/article.js
+++ Master-frameworks-js/Backend/controllers/article.js
@@ -1,25 +1,190 @@
 // Diferentes metodos y rutas relacionadas con nuestra API
-'use strict'
+"use strict";
 
-var controller ={
+const { query } = require("express");
+var validator = require("validator");
+const { default: isTaxID } = require("validator/lib/isTaxID");
+const article = require("../models/article");
+var Article = require("../models/article");
 
-    datosCurso: (req,res)=>{
-    
-        var hola = req.body.hola;
-    
-        return res.status(200).send({
-            curso: 'Master en Frameworks JS',
-            author: 'Julio Velezmoro',
-            url: 'google.com',
-            hola
-        });
-    },
+var controller = {
+  datosCurso: (req, res) => {
+    var hola = req.body.hola;
 
-    test: (req,res) => {
-        return res.status(200).send({
-            message: 'Soy la accion test de mi controlador de articulos'
-        });
+    return res.status(200).send({
+      curso: "Master en Frameworks JS",
+      author: "Julio Velezmoro",
+      url: "google.com",
+      hola,
+    });
+  },
+
+  test: (req, res) => {
+    return res.status(200).send({
+      message: "Soy la accion test de mi controlador de articulos",
+    });
+  },
+
+  save: (req, res) => {
+    // Recoger parametros por post
+    var params = req.body;
+    console.log(params);
+
+    // Validar datos(validator)
+    try {
+      var validate_title = !validator.isEmpty(params.title);
+      var validate_content = !validator.isEmpty(params.content);
+    } catch (err) {
+      return res.status(200).send({
+        status: "error",
+        message: "Faltan datos por enviar",
+      });
     }
+
+    if (validate_content && validate_title) {
+      // Crear el objeto a guardar
+      var article = new Article();
+
+      // Asignar valores
+      article.title = params.title;
+      article.content = params.content;
+      article.image = null;
+
+      // Guardar articulo
+      article.save((err, articleStored) => {
+        if (err || !articleStored) {
+          return res.status(404).send({
+            status: "error",
+            message: "El articulo no se ha guardado",
+          });
+        }
+        // Devolver respuesta
+        return res.status(200).send({
+          status: "success",
+          article: articleStored,
+        });
+      });
+    } else {
+      return res.status(200).send({
+        status: "error",
+        message: "Los datos no son validos",
+      });
+    }
+  },
+
+  getArticles: (req, res) => {
+    var query = Article.find({});
+
+    var last = req.params.last;
+    if (last || last != undefined) {
+      query.limit(5);
+    }
+
+    // Find
+    query.sort("-da2te").exec((err, articles) => {
+      if (err) {
+        return res.status(500).send({
+          status: "error",
+          message: "Error al devolver los articulos",
+        });
+      }
+      if (!articles) {
+        return res.status(404).send({
+          status: "error",
+          message: "Articulos no existen",
+        });
+      }
+
+      return res.status(200).send({
+        status: "success",
+        articles,
+      });
+    });
+  },
+
+  getArticle: (req, res) => {
+    // Recoger el id del url
+    var articleId = req.params.id;
+    // Comprobar que existen
+    if (!articleId || articleId == null) {
+      return res.status(404).send({
+        status: "error",
+        message: "No existe el articulo",
+      });
+    }
+    // Buscar el articulo
+    Article.findById(articleId, (err, article) => {
+      if (err || !article) {
+        return res.status(404).send({
+          status: "error",
+          message: "No existe el articulo",
+        });
+      }
+      // Devolver un Json
+      return res.status(404).send({
+        status: "success",
+        article,
+      });
+    });
+  },
+
+  update: (req, res) => {
+    // Recoger el id del articulo por la url
+    var articleId = req.params.id;
+
+    // Recoger los datos que llegan por put
+    var params = req.body;
+
+    // Validar datos
+    try {
+      var validate_title = !validator.isEmpty(params.title);
+      var validate_content = !validator.isEmpty(params.content);
+    } catch (err) {
+      return res.status(200).send({
+        status: "error",
+        message: "Faltan datos por enviar",
+      });
+    }
+    if (validate_title && validate_content) {
+      // Find ad update
+      Article.findOneAndUpdate(
+        { _id: articleId },
+        params,
+        { new: true },
+        (err, articleUpdated) => {
+          if (err) {
+            return res.status(500).send({
+              status: "error",
+              message: "Error al actualizar",
+            });
+          }
+          if (!articleUpdated) {
+            return res.status(404).send({
+              status: "error",
+              message: "No existe el articulo",
+            });
+          }
+          return res.status(200).send({
+            status: "success",
+            article: articleUpdated,
+          });
+        }
+      );
+    } else {
+      // Devolver respuesta
+      return res.status(200).send({
+        status: "error",
+        message: "La validacion no es correcta",
+      });
+    }
+  },
+  
+  delete: (req, res) =>{
+    return res.status(200).send({
+        status: "error",
+        message: "Faltan datos por enviar",
+      }); 
+  }
 };
 // End controller
 
--- Master-frameworks-js/Backend/routes/article.js
+++ Master-frameworks-js/Backend/routes/article.js
@@ -7,7 +7,15 @@ var articleController = require('../controllers/article');
 
 var router = express.Router()
 
+// Rutas de prueba
 router.post('/datos-curso',articleController.datosCurso);
 router.get('/test-controller',articleController.test);
+
+// Rutas utiles
+router.post('/save',articleController.save);
+router.get('/articles/:last?',articleController.getArticles);
+router.get('/article/:id',articleController.getArticle);
+router.put('/article/:id',articleController.update);
+router.delete('/article/:id',articleController.delete);
 
 module.exports = router;
