c045a563752aabff829b41bfb655e12148d92f9d
Lakorthus <julio28ve@gmail.com>
2022-08-06 17:10:41 +0100

    modelos de angular

---
+++ Master-frameworks-js/aprendiendoAngular/src/app/components/models/pelicula.ts
@@ -0,0 +1,8 @@
+export class Pelicula{
+   
+    constructor(public title:string, public year:number , public image:string){
+        this.title= title;
+        this.year= year;
+        this.image=image
+    }
+}
--- Master-frameworks-js/aprendiendoAngular/src/app/components/peliculas/peliculas.component.ts
+++ Master-frameworks-js/aprendiendoAngular/src/app/components/peliculas/peliculas.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, DoCheck, OnDestroy } from '@angular/core';
-
+import { Pelicula } from '../models/pelicula';
 @Component({
   selector: 'peliculas',
   templateUrl: './peliculas.component.html',
@@ -7,36 +7,52 @@ import { Component, OnInit, DoCheck, OnDestroy } from '@angular/core';
 })
 export class PeliculasComponent implements OnInit, DoCheck, OnDestroy {
   public titulo: string;
-  public peliculas: Array<any>;
+  public peliculas: Pelicula[];
 
   constructor() {
     this.titulo = 'Peliculas funciona';
 
     this.peliculas = [
-      { 
-        year:2022,
-        title: 'Spiderman 4',
-        image:
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_Ee-gTAKvTR3QIy5yO91FEh8r6mFL82Efl9NeKr_SJ_sHaHoLUdBr9mPpXQSW_kAW8FY&usqp=CAU',
-      },
-      { 
-        year:2020,
-        title: 'Avengers',
-        image:
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNyfbWV_X1cmcVAx-MzZd_Lgkv3lzA5XyZnA&usqp=CAU',
-      },
-      { 
-        year:2018,
-        title: 'Superman',
-        image:
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgWCsNUhGxi0Lg1e_uShhRhIHRYuhUjYJMhg&usqp=CAU' 
-          
-      },
+      new Pelicula(
+        'Spiderman 4',
+        2022,
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_Ee-gTAKvTR3QIy5yO91FEh8r6mFL82Efl9NeKr_SJ_sHaHoLUdBr9mPpXQSW_kAW8FY&usqp=CAU'
+      ),
+      new Pelicula(
+        'Avengers',
+        2020,
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNyfbWV_X1cmcVAx-MzZd_Lgkv3lzA5XyZnA&usqp=CAU'
+      ),
+      new Pelicula(
+        'Sperman',
+        2018,
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgWCsNUhGxi0Lg1e_uShhRhIHRYuhUjYJMhg&usqp=CAU'
+      ),
+
+      // {
+      //   year:2022,
+      //   title: 'Spiderman 4',
+      //   image:
+      //     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_Ee-gTAKvTR3QIy5yO91FEh8r6mFL82Efl9NeKr_SJ_sHaHoLUdBr9mPpXQSW_kAW8FY&usqp=CAU',
+      // },
+      // {
+      //   year:2020,
+      //   title: 'Avengers',
+      //   image:
+      //     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNyfbWV_X1cmcVAx-MzZd_Lgkv3lzA5XyZnA&usqp=CAU',
+      // },
+      // {
+      //   year:2018,
+      //   title: 'Superman',
+      //   image:
+      //     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgWCsNUhGxi0Lg1e_uShhRhIHRYuhUjYJMhg&usqp=CAU'
+
+      // },
     ];
   }
 
   ngOnInit(): void {
-    console.log(this.peliculas)
+    console.log(this.peliculas);
     console.log('Componente iniciado');
   }
 
